a1a8fc4e4a1844607da643a45d07460af34167b5
hb9cwp <rolf.sommerhalder@alumni.ethz.ch>
2014-04-30 12:29:58 +0200

    format (Tabs), test scaffolding

--- public/javascripts/ng-planetaryjs.js
+++ public/javascripts/ng-planetaryjs.js
@@ -13,14 +13,15 @@
 				}			
 
 			};
-        }])
+		}])
 
 		.directive('planetaryjs', ['service', function (service) {
+//.directive('planetaryjs', [function () {
 
 			return {
 				restrict : 'AE',
-				scope: { options: '=' },				
-				link : function(scope, element, attrs) {	
+				scope: { options:'=' },
+				link : function(scope, element, attrs) {
 
 					var canvas = element[0];
 					var middle = canvas.width/2;
@@ -59,14 +60,21 @@
 						globe.stop();		
 					};
 
-					draw();						   				   															
-					
+					draw();
+
 					var addPings = function(lng, lat, config) {
 						if (globe.plugins.pings)
 							globe.plugins.pings.add(lng, lat, config);
 					};
 
-					var stop;
+//element.on('mousedown', function(event) {
+// addPings(12, 30);
+//});
+
+element.on('$destroy', function() {
+ //globe.stop();
+ drawStop();
+});
 
 					// Listener
 					scope.$on('add-ping', function(event, msg){
@@ -84,58 +92,52 @@
    							stop = !stop;
    						}
  					});
-					
-					element.on('$destroy', function() {
-						globe.stop();
-					});
-					
+
 					function autorotate(degPerSec) {
-		                // Planetary.js plugins are functions that take a `planet` instance
-		                // as an argument...
-		                return function(planet) {
-		                  var lastTick = null;
-		                  var paused = false;
-		                  planet.plugins.autorotate = {
-		                    pause:  function() { paused = true;  },
-		                    resume: function() { paused = false; }
-		                  };
-		                  // ...and configure hooks into certain pieces of its lifecycle.
-		                  planet.onDraw(function() {		                  	
-
-		                    if (paused || !lastTick) {
-		                      lastTick = new Date();
-		                    } else {
-		                      var now = new Date();
-		                      var delta = now - lastTick;
-		                      // This plugin uses the built-in projection (provided by D3)
-		                      // to rotate the globe each time we draw it.
-		                      var rotation = planet.projection.rotate();
-		                      rotation[0] += degPerSec * delta / 1000;
-		                      if (rotation[0] >= 180) rotation[0] -= 360;
-		                      planet.projection.rotate(rotation);
-		                      lastTick = now;
-		                    }
-		                  });
-		                };
-		              };  
-
-				    scope.results = [];
-				    				    				                       
-					scope.fetchIt = function(type, result) {
-					    
-                     	//ngProgress.start();
-
-						useragent.asyncFetchAgents( type , scope.results).then(function(d) {
-						    scope.results = [];
-						    for (var i=0;i<d.data.length;i++) {
-						        scope.results.push({id:i, agent: d.data[i]});
-						    }
-							//ngProgress.stop();
-							//timeout(ngProgress.complete(), 800);
+						// Planetary.js plugins are functions that take a `planet` instance
+						// as an argument...
+						return function(planet) {
+							var lastTick = null;
+							var paused = false;
+							planet.plugins.autorotate = {
+								pause:  function() { paused = true;  },
+								resume: function() { paused = false; }
+							};
+							// ...and configure hooks into certain pieces of its lifecycle.
+							planet.onDraw(function() {
+							if (paused || !lastTick) {
+								lastTick = new Date();
+							} else {
+								var now = new Date();
+								var delta = now - lastTick;
+								// This plugin uses the built-in projection (provided by D3)
+								// to rotate the globe each time we draw it.
+								var rotation = planet.projection.rotate();
+								rotation[0] += degPerSec * delta / 1000;
+								if (rotation[0] >= 180) rotation[0] -= 360;
+								planet.projection.rotate(rotation);
+								lastTick = now;
+							}
 						});
 					};
-								
-				}
-			};
+				};
+
+				scope.results = [];
+				    				    				                       
+				scope.fetchIt = function(type, result) {
+					    
+                     			//ngProgress.start();
+
+					useragent.asyncFetchAgents( type , scope.results).then(function(d) {
+						scope.results = [];
+						for (var i=0;i<d.data.length;i++) {
+							scope.results.push({id:i, agent: d.data[i]});
+						}
+						//ngProgress.stop();
+						//timeout(ngProgress.complete(), 800);
+					});
+				};							
+			}
+		};
 	}]);
 })(angular);
